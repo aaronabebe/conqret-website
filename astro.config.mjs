f6eb2b78e50173471e6abbebb0aabc5c5d273e2f
Aaron Abebe <aaron.abebe@gmail.com>
2023-03-11 12:11:47 +0100

    updated base in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import node from "@astrojs/node";
 // https://astro.build/config
 export default defineConfig({
   site: 'aaronabebe.github.io',
-  base: '/conqret-website',
   output: 'server',
   adapter: node({
     mode: "standalone"
